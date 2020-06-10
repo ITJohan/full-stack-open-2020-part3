8edebfb50c9e5ed890d8e99d1f50db087e661f94
ITJohan <johanlindkvist89@gmail.com>
2020-06-10 15:40:16 +0200

    Add 3.11

---
+++ build/precache-manifest.54cbaaa457bb5a5178ee2f071b5dff42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04f3c7e395914e4b92433338dda44ebd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e07caed86d8f68a024d",
+    "url": "/static/js/2.eb57948a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.eb57948a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04c7d3476837ccebfba9",
+    "url": "/static/js/main.32771677.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
